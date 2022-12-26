173b4f8d3ce898db0361a50f35c48c005d47ad2c
Danielazo2021 <89363559+Danielazo2021@users.noreply.github.com>
2022-12-26 14:05:35 -0300

    85%

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent  implements OnInit{
+  public title: string;
+  public subtitle: string;
+  public email: string;
+  constructor(){
+    this.title= "Daniel Grecco";
+    this.subtitle= "Desarrollador web";
+    this.email="danielalbertogrecco@gmail.com"
+  }
+  ngOnInit(): void {
+    
+  }
 
 }
